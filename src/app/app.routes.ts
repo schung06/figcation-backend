606b4855554c5224c34ca2e8dba7a790398d253b
Stephanie <stephaniec328122716@gmail.com>
2025-08-27 12:22:00 -0700

    route recs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard/dashboard';
 import { Calendar } from './pages/calendar/calendar';
 import { Playground } from './pages/playground/playground';
+import { Recommendations } from './pages/recommendations/recommendations/recommendations';
 
 export const routes: Routes = [
     { path: '', component: Dashboard }, // Home route
     { path: 'dashboard', component: Dashboard },
     { path: 'calendar', component: Calendar },
+    { path: 'recommendations', component: Recommendations },
     { path: 'playground', component: Playground }
 ];
