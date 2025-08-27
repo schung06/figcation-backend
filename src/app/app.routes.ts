2f18c7a84c679537bfe649a2457e6657ec508585
Stephanie <stephaniec328122716@gmail.com>
2025-08-26 20:35:00 -0700

    added routing for dashboard and itinerary calendar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './pages/dashboard/dashboard/dashboard';
+import { Calendar } from './pages/calendar/calendar';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Dashboard }, // Home route
+    { path: 'dashboard', component: Dashboard },
+    { path: 'calendar', component: Calendar }
+];
--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css'
 })
