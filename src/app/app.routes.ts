2bca02ccec89af11221a44ab2a874dc88c4fe804
Stephanie <stephaniec328122716@gmail.com>
2025-08-27 11:52:52 -0700

    add toolbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './pages/dashboard/dashboard/dashboard';
 import { Calendar } from './pages/calendar/calendar';
+import { Playground } from './pages/playground/playground';
 
 export const routes: Routes = [
     { path: '', component: Dashboard }, // Home route
     { path: 'dashboard', component: Dashboard },
-    { path: 'calendar', component: Calendar }
+    { path: 'calendar', component: Calendar },
+    { path: 'playground', component: Playground }
 ];
--- src/app/pages/playground/playground.ts
+++ src/app/pages/playground/playground.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Toolbar } from "./toolbar/toolbar";
 
 @Component({
   selector: 'app-playground',
-  imports: [],
+  imports: [CommonModule, Toolbar],
   templateUrl: './playground.html',
   styleUrl: './playground.css'
 })
 export class Playground {
 
+  beaches = [
+    {
+      name: 'Beach',
+      locations: ['PB Beach', 'La Jolla Shores', 'Wind and Sea']
+    },
+    {
+      name: 'Beach',
+      locations: ['PB Beach', 'La Jolla Shores', 'Wind and Sea']
+    },
+    {
+      name: 'Beach',
+      locations: ['PB Beach', 'La Jolla Shores', 'Wind and Sea']
+    }
+  ];
 }
