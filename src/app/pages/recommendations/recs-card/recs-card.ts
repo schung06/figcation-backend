3f2e58e7dfca25fca4176b358819c632e24653d0
Stephanie <stephaniec328122716@gmail.com>
2025-08-27 13:42:30 -0700

    update recommendations

---
+++ src/app/pages/recommendations/recs-card/recs-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recs-card',
+  imports: [],
+  templateUrl: './recs-card.html',
+  styleUrl: './recs-card.css'
+})
+export class RecsCard {
+
+}
